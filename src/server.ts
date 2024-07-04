9ed7ed37bad11d693a93a8fe3df33f19e74d8d06
JoelVR17 <vermudo.com@gmail.com>
2024-07-04 16:17:22 -0600

    feat: Add CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 
 // DB
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors(corsConfig));
 app.use(express.json());
 
 // Routes
