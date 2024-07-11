054a069f09a03e18b0e41e0fd6c654962a898f17
JoelVR17 <vermudo.com@gmail.com>
2024-07-11 15:24:57 -0600

    update: Set the URL in order to confirm the account

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -17,7 +17,7 @@ export class AuthEmail {
         <p>Hello: ${user.name}, your account has been created with Home Task. We are almost done, you just have to confirm your account.</p>
         
         <p>Click here:</p>
-        <a href="">Confirm your Account</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirm your Account</a>
         <p>Enter the Code: <b>${user.token}</b></p>
         <p>This token expires in 10 minutes</p>
 
