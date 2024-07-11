abef1aab61c359b372c238f10b4b3e810dee1662
JoelVR17 <vermudo.com@gmail.com>
2024-07-11 15:56:25 -0600

    feat: Create the endpoint to request new code

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -39,4 +39,11 @@ router.post(
   AuthController.login
 );
 
+router.post(
+  "/request-code",
+  body("email").isEmail().withMessage("Invalid Email"),
+  handleInputErrors,
+  AuthController.requestConfirmationCode
+);
+
 export default router;
