2fecf033de0b49b8b45fd69e93df4b1d9d0cfdc0
JoelVR17 <vermudo.com@gmail.com>
2024-07-12 15:33:25 -0600

    feat: Add generation of JWT

--- src/models/Token.ts
+++ src/models/Token.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 export interface IToken extends Document {
   token: string;
   user: Types.ObjectId;
-  createdAt: Date;
+  expiresAt: Date;
 }
 
 const tokenSchema: Schema = new Schema({
---
+++ src/utils/jwt.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import Types from "mongoose";
+
+type UserPaylod = {
+  id: Types.ObjectId;
+};
+
+export const generateJWT = (payload: UserPaylod) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "6m",
+  });
+
+  return token;
+};
