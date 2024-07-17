650055f60fd1acdb67766c899c2f39dcf34e1a94
JoelVR17 <vermudo.com@gmail.com>
2024-07-17 15:36:44 -0600

    feat: Add update of profile and password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -219,4 +219,49 @@ export class AuthController {
   static user = async (req: Request, res: Response) => {
     return res.json(req.user);
   };
+
+  static updateProfile = async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const userExist = await User.findOne({ email });
+
+    if (userExist && userExist.id.toString() !== req.user.id.toString()) {
+      const error = new Error("This email is taken");
+      return res.status(409).json({ error: error.message });
+    }
+
+    req.user.name = name;
+    req.user.email = email;
+
+    try {
+      await req.user.save();
+      res.send("Profile updated successfully");
+    } catch (error) {
+      res.status(500).json({ error: "Error" });
+    }
+  };
+
+  static updateCurrentUserPassword = async (req: Request, res: Response) => {
+    const { current_password, password } = req.body;
+
+    const user = await User.findById(req.user.id);
+
+    const isPasswordCorrect = await checkPassword(
+      current_password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      const error = new Error("Current password is incorrect");
+      return res.status(401).json({ error: error.message });
+    }
+
+    try {
+      user.password = await hashPassword(password);
+
+      await user.save();
+      res.send("Password updated successfully");
+    } catch (error) {
+      res.status(500).json({ error: "Error" });
+    }
+  };
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -79,4 +79,35 @@ router.post(
 
 router.get("/user", authenticate, AuthController.user);
 
+/**
+ * Profile
+ */
+router.put(
+  "/profile",
+  authenticate,
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").isEmail().withMessage("Invalid Email"),
+  handleInputErrors,
+  AuthController.updateProfile
+);
+
+router.post(
+  "/update-password",
+  authenticate,
+  body("current_password")
+    .notEmpty()
+    .withMessage("Current password is required"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password is too short, must be at least 8 characters"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("The passwords do not match");
+    }
+    return true;
+  }),
+  handleInputErrors,
+  AuthController.updateCurrentUserPassword
+);
+
 export default router;
